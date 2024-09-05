17d24a2d94cd4e9d25cf5756f3befeec5f97f17e
djiesr <31359825+djiesr@users.noreply.github.com>
2024-09-05 16:35:37 -0400

    update

--- custom_components/planethoster/__init__.py
+++ custom_components/planethoster/__init__.py
@@ -1,9 +1,10 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
-from .const import DOMAIN
+from .const import DOMAIN  # Importer le domaine depuis const.py
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data[DOMAIN] = PlanetHosterAPI(entry.data["api_key"], entry.data["api_user"])
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+async def async_setup_entry(hass, config_entry):
+    # Utiliser DOMAIN pour stocker les données
+    hass.data[DOMAIN] = PlanetHosterAPI(config_entry.data["api_key"], config_entry.data["api_user"])
+    hass.config_entries.async_setup_platforms(config_entry, ["sensor"])
     return True
