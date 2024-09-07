c6d801c2c7b2e591d48d39c1576b3242822eb07c
djiesr <31359825+djiesr@users.noreply.github.com>
2024-09-07 08:35:08 -0400

    update

--- custom_components/planethoster/__init__.py
+++ custom_components/planethoster/__init__.py
@@ -9,7 +9,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up PlanetHoster from a config entry."""
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
