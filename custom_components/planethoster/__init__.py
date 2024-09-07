79e783c81c89d65646c6e5135b764c1b8d272e3d
djiesr <31359825+djiesr@users.noreply.github.com>
2024-09-07 07:15:03 -0400

    update

--- custom_components/planethoster/__init__.py
+++ custom_components/planethoster/__init__.py
@@ -1,1 +1,9 @@
-# __init__.py
+"""PlanetHoster Integration."""
+from homeassistant.helpers.entity import Entity
+
+DOMAIN = "planethoster"
+
+def setup(hass, config):
+    """Set up the PlanetHoster integration."""
+    hass.states.set(f"{DOMAIN}.test_sensor", 100)
+    return True
--- custom_components/planethoster/sensor.py
+++ custom_components/planethoster/sensor.py
@@ -1,18 +1,23 @@
+"""Sensor for PlanetHoster."""
 from homeassistant.helpers.entity import Entity
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the PlanetHoster sensors."""
-    async_add_entities([PlanetHosterTestSensor()], True)
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the PlanetHoster sensor."""
+    add_entities([PlanetHosterTestSensor()])
 
 class PlanetHosterTestSensor(Entity):
-    """Representation of a static test sensor for PlanetHoster."""
+    """Representation of a test sensor for PlanetHoster."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = 100
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return "sensor.planethoster_check"
+        return "PlanetHosterTestSensor"
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return 100
+        return self._state
